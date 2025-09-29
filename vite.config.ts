66778342049c2582408e97d67d55d0ad10e72e53
P202U <156369604+P202U@users.noreply.github.com.>
2025-09-29 09:49:22 +0530

    Add caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   manifest: {
-    //     name: 'SaralIntern: AI internship recommendation system',
-    //     short_name: 'SaralIntern',
-    //     start_url: '.',
-    //     display: 'standalone',
-    //     background_color: '#ffffff',
-    //     theme_color: '#4a90e2',
-    //     icons: [
-    //       {
-    //         src: 'icons/icon-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: 'icons/icon-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,png,svg}'],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SaralIntern: AI internship recommendation system',
+        short_name: 'SaralIntern',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4a90e2',
+        icons: [
+          {
+            src: '/favicon/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png}'],
+      },
+    }),
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
